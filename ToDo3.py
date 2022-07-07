eca68e1be2cc7e83d4637e1ac811966f564b2c57
ca.morallesb@gmail.com <ca.morallesb@gmail.com>
2022-07-07 11:51:32 -0300

    Adicionada a função de inserir notas de novos candidatos

--- ToDo3.py
+++ ToDo3.py
@@ -1,7 +1,8 @@
-resultados = ['e5_t10_p8_s8', 'e10_t7_p7_s8', 'e8_t5_p4_s9', 'e2_t2_p2_s1', 'e10_t10_p8_s9', 'e8_t5_p6_s9']
+resultados = ['e5_t10_p8_s8', 'e10_t7_p7_s8', 'e8_t5_p4_s9',
+              'e2_t2_p2_s1', 'e10_t10_p8_s9', 'e8_t5_p6_s9']
 
 
-def buscar_notas(resultados):
+def receberNotasMinimas():
     notas_min = []
     nota_e_min = (input("Nota mínima na entrevista: "))
     notas_min.append(nota_e_min)
@@ -11,9 +12,31 @@ def buscar_notas(resultados):
     notas_min.append(nota_p_min)
     nota_s_min = (input("Nota mínima na avaliação soft skills: "))
     notas_min.append(nota_s_min)
+    
+    # if (nota_e_min != int(nota_e_min)) or (nota_t_min != int(nota_t_min)) or (nota_p_min != int(nota_p_min)) or (nota_s_min != int(nota_s_min)):
+    #     print(f'Insira apenas números inteiros válidos.')
+    #     return receberNotasMinimas()
+    # else:
+    return notas_min
 
+
+def atualizarListaDeNotas(resultados):
+    entrada = input(
+        'Deseja adicionar mais algum candidato ao banco de dados? Digite "SIM" ou "NÃO" ')
+
+    if (entrada.upper() == 'NÃO') or (entrada.upper() == 'NAO'):
+        return resultados
+    if entrada.upper() == 'SIM':
+        entrada1 = input('Insira a nota no formato "eW_tX_pY_sZ"')
+        resultados.append(entrada1)
+        return resultados
+    else:
+        print(f'Digite uma opção válida: SIM ou NÃO')
+    return atualizarListaDeNotas(resultados)
+
+
+def listarCandidatos(resultados):
     notas_candidatos = []
-
     for i in resultados:
         notas_individuais = []
         e = i[i.index('e') + 1: i.index("_")]
@@ -26,8 +49,11 @@ def buscar_notas(resultados):
         notas_individuais.append(s)
 
         notas_candidatos.append(notas_individuais)
+    return notas_candidatos
 
-        candidatos_aptos = []
+
+def candidatosAptos(notas_candidatos, notas_min):
+    candidatos_aptos = []
 
     for notas_i in range(len(notas_candidatos)):
         notas = notas_candidatos[notas_i]
@@ -38,10 +64,22 @@ def buscar_notas(resultados):
 
     if candidatos_aptos == []:
         return print(f'Nenhum candidato atende aos critérios escolhidos')
-    else:    
+    else:
         for i in candidatos_aptos:
             print(f'O candidato {i} atende aos critérios escolhidos.')
-    
-    return 
+    return
 
-(buscar_notas(resultados))
+
+# Iniciando o programa:
+
+# Atualizando, ou não, a lista que contém as notas dos candidatos entrevistados:
+resultados = atualizarListaDeNotas(resultados)
+
+# Transforma o modelo inicial de armazenamento de notas em apenas números:
+notas_candidatos = listarCandidatos(resultados)
+
+# Recebe quais os critério utilizados como mínimos:
+notas_min = receberNotasMinimas()
+
+# Diz, por fim, quais candidatos estão aptos de acordo com os requisitos mínimos
+candidatosAptos(notas_candidatos, notas_min)
